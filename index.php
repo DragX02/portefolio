bfffa7c8eef93f99f03af7326836e149491ccb9e
johan <166138951+DragX02@users.noreply.github.com>
2025-10-01 19:39:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$metaDescription = "description de la page actuelle...";
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'header.php';
+?>
+<h2>Bienvenue sur votre site web !</h2>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime recusandae, enim voluptate ea optio iusto incidunt. Vel vero accusamus eligendi ab dolores, in doloremque, eveniet ipsa numquam eaque suscipit minus?</p>
+<p>Modi ex consequatur aperiam, assumenda officiis quae neque laboriosam. Veniam quod laudantium ratione facilis, quidem unde quis consequatur! Cupiditate eaque vero asperiores dolorem cum rerum nam, voluptatem suscipit exercitationem fuga?</p>
+<?php require_once __DIR__ . DIRECTORY_SEPARATOR . 'footer.php'; ?>
