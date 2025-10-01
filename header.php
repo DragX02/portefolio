9f2f3530db575feb16ab1af5c005604938bf6dba
johan <166138951+DragX02@users.noreply.github.com>
2025-10-01 20:57:58 +0200

    change img

--- header.php
+++ header.php
@@ -4,5 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo isset($metaDescription) ? htmlspecialchars($metaDescription) : 'Mon Premier Modèle de Page Dynamique'; ?>">
-    <title>Mon Premier Modèle de Page Dynamique</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>portfolio Olbrechts johan</title>
+    <link rel="stylesheet" href="css\style.css">
+</head>
+<body>
+    <header>
+        <h1>Mon Premier Modèle de Page Dynamique</h1>
+        <nav>
+            <ul>
+             <li class="nav-item">
+                <a class="nav-link" href="index.php" aria-label="Accueil">
+                        <img class="home-icon" src="Image\logo\accueil.png"  alt="Accueil">
+                                    <span class="nav-text">Accueil</span></a></li>
+                                    <li class="nav-item">
+                <a class="nav-link" href="index.php" aria-label="À Propos">
+                        <img class="home-icon" src="Image\logo\About2.png"  alt="Á Propos">
+                                    <span class="nav-text">Á Propos</span></a></li>
+                
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
